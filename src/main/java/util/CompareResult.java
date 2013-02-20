edf04a0dba3912019d37c8a6deb092527b368201
Antony Teoh <antonyteoh@Antonys-MacBook-Pro.local>
2013-02-20 15:27:00 +0000

    Fixing files, adding CompareUtil to compare list of maps

---
+++ src/main/java/util/CompareResult.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: antonyteoh
+ * Date: 20/02/2013
+ * Time: 15:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompareResult {
+}
---
+++ src/main/java/util/CompareUtil.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: antonyteoh
+ * Date: 20/02/2013
+ * Time: 15:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompareUtil {
+}
